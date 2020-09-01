852c7984d0105ada63b1588efe2ada2f2e72ad27
Obinna Ogbonna <oogbonna@gidimobile.com>
2020-09-01 08:59:28 +0100

    initial commit
    use interpret to create service for xstate

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import { createMachine, interpret } from 'xstate';
+
+const elBox = document.querySelector('#box');
+
+const machine = createMachine({
+    // Create your state machine here
+    // ...
+    initial: 'inactive',
+    states: {
+        inactive: {
+            on: {
+                mousedown: {
+                    target: 'active',
+                },
+            },
+        },
+        active: {
+            on: {
+                mouseup: {
+                    target: 'inactive',
+                },
+            },
+        },
+    }
+});
+
+// Create a service using interpret(...)
+const service = interpret(machine);
+
+
+// Listen to state transitions and set
+// `elBox.dataset.state` to the state value as before.
+// ...
+service.onTransition((state) => {
+    elBox.dataset.state = state.value;
+});
+
+// Start the service.
+// ...
+service.start();
+
+elBox.addEventListener('mousedown', (event) => {
+    // Send a mousedown event
+    // ...
+    service.send(event);
+});
+
+elBox.addEventListener('mouseup', (event) => {
+    // Send a mouseup event
+    // ...
+    service.send(event);
+});
